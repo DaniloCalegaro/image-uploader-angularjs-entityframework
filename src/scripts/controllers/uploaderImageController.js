84af7e607e3bbb67c982743acfc7113ad98298a9
DaniloCalegaro <danilo.calegaro@hotmail.com>
2023-03-04 04:33:34 -0300

    feat: creating input image

--- src/scripts/controllers/uploaderImageController.js
+++ src/scripts/controllers/uploaderImageController.js
@@ -2,4 +2,10 @@ var app = angular.module("app", ["ngRoute"]);
 
 app.controller("uploaderImageController", function ($scope) {
   $scope.app = "Tela 1";
+  $scope.image = null;
+  $scope.imageFileName = '';
+  
+  $scope.uploadme = {};
+  $scope.uploadme.src = '';
 })
+
